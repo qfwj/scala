c39c5a90898f174e9d6d9cf4c1bc7313266ed94e
qf <13554254100@163.com>
2018-11-28 16:21:57 +0800

    add implicit

---
+++ scalatest/src/covariance.scala
@@ -0,0 +1,48 @@
+import CovariantAndContravariantDemo._
+
+
+class sup
+
+class son extends sup
+
+class sonson
+
+class test[+T] {
+  def test[u >: T](x: u): Unit = {
+    println(x)
+  }
+}
+
+
+/*class c1[+T] {
+
+  def method[K >: T](x: K) {println(x)}
+}
+class c2[T] extends c1[T]*/
+
+trait c1[T] {
+
+  def method[K >: T](x: K) = x
+}
+
+object c2 extends c1[Int]
+
+
+object CovariantAndContravariantDemo {
+  def main(args: Array[String]): Unit = {
+
+    c2.method(3) // 3
+
+    c2.method(3.0) // 3.0
+
+    c2.method("abc") // "abc"
+
+    val son1: son = new son
+    val sup1: sup = new sup
+    val sonson1: sonson = new sonson
+
+    val test11: test[son] = new test[son]
+    test11.test(sonson1)
+  }
+
+}
---
+++ scalatest/src/dsd.scala
@@ -0,0 +1,11 @@
+/**
+  * @ClassName: dsd
+  * @Description:
+  * @author: zhbo
+  * @date: 2018/11/28 下午3:49
+  * @Copyright: 2017 . All rights reserved.
+  *
+  */
+object JoesPrefs {
+  implicit val prompt = new PreferredPrompt("Yes, master> ")
+}
