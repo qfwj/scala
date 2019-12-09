86b127ae3d2c6ec9886a5994000f4cdb5231687e
qfwj <13554254100@163.com>
2019-12-09 14:24:38 +0800

    hdfs

--- sparktest/src/main/javasss/HdfsTest.java
+++ sparktest/src/main/javasss/HdfsTest.java
@@ -18,26 +18,10 @@ public class HdfsTest {
 
     public static void main(String[] args)  throws Exception {
         Configuration conf = new Configuration();
-
-        //这里指定使用的是 hdfs文件系统
-        //conf.set("fs.defaultFS", "hdfs://master:9000");
-
         conf.set("dfs.client.use.datanode.hostname", "true");
-        //通过这种方式设置java客户端身份
-        // System.setProperty("HADOOP_USER_NAME", "root");
-        //FileSystem fs = FileSystem.get(conf);
         //或者使用下面的方式设置客户端身份
         FileSystem fs = FileSystem.get(new URI("hdfs://master:9000"),conf,"root");
-
-        // fs.create(new Path("/helloByJava")); //创建一个目录
-
-        //文件下载到本地 如果出现0644错误或找不到winutils.exe,则需要设置windows环境和相关文件.
-        //fs.copyToLocalFile(new Path("/zookeeper.out"), new Path("D:\\test\\examplehdfs"));
-
-
-        //使用Stream的形式操作HDFS，这是更底层的方式
-      fs.copyToLocalFile(false,new Path("/user/test.txt"),new Path("C:\\Users\\nefu_\\Desktop\\tttt"));
-
+        fs.copyToLocalFile(false,new Path("/user/test.txt"),new Path("C:\\Users\\nefu_\\Desktop\\tttt"));
         fs.close();
         System.out.println("");
     }
