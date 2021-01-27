3b633a81287639694f336222e6890f6b2a880918
qf <13554254100@163.com>
2021-01-27 23:00:18 +0800

    add scala

--- springboot/src/main/scala/com/qf/concurrent/TestFuture.scala
+++ springboot/src/main/scala/com/qf/concurrent/TestFuture.scala
@@ -1,21 +1,30 @@
 package com.qf.concurrent
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
 
 /**
  * description:   https://docs.scala-lang.org/overviews/core/futures.html#the-global-execution-context
  * date: 2021/1/22 9:21 
  * author: zhbo 
  */
-object TestFuture  extends App{
+object TestFuture extends App {
 
-  val ff =  Future {
+  val ff = Future {
     println(Thread.currentThread() + "" + 12)
     12
   }
+  ff onComplete {
+    case Failure(exception) => println(s"some Exception: ${exception}")
+    case Success(r) => println(s"some result: ${r}")
+  }
+  val re = ff andThen {
+    case Success(value) => 33
+    case Failure(exception) => 0
+  }
+    re.foreach(f => println(f))
 
-  ff foreach( f => {
-    println(f)
-  })
+  Await.result(ff, 12 nanos)
 }
